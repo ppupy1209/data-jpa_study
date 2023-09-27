38c58be9ddb0a13b066f6b54af7f976f4bb8ed90
yeonwoo <ppupy1209@naver.com>
2023-09-27 16:21:48 +0900

    @Query, 쿼리 정의

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,10 +1,17 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+         List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
+         @Query("select m from Member m where m.username = :username and m.age= :age")
+         List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
