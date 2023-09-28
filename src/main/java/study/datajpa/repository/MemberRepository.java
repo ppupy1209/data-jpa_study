6f6a3e81dd56823cdba42174d8eabb8bb1830f57
yeonwoo <ppupy1209@naver.com>
2023-09-28 09:44:44 +0900

    fetch join, @EntityGraph

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import javax.persistence.Entity;
 import java.util.List;
 
 @Repository
@@ -36,4 +38,19 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
          @Modifying(clearAutomatically = true)
          @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
          int bulkAgePlus(@Param("age") int age);
+
+         @Query("select m from Member m" +
+                 " join fetch m.team")
+         List<Member> findMemberFetchJoin();
+
+         @Override
+         @EntityGraph(attributePaths = {"team"})
+         List<Member> findAll();
+
+         @EntityGraph(attributePaths = {"team"})
+         @Query("select m from Member m")
+         List<Member> findMemberEntityGraph();
+
+         @EntityGraph(attributePaths = {"team"})
+         List<Member> findEntityGraphByUsername(String username);
 }
